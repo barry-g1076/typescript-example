7f61ba22539a82e17c2a6b0ba97307a09bc26ff8
Barrrington Patterson <barry545young@gmail.com>
2023-02-07 18:59:54 -0500

    initial commit

---
+++ src/store.ts
@@ -0,0 +1,17 @@
+export interface Dog{
+    name: string;
+    age: number;
+    breed?:string;
+}
+
+// const dog: Dog={
+//     name:"jack",
+//     age: 12
+// }
+
+export function getGoodDogs():Array<Dog>{
+    return[
+        {name: 'Sammy', age: 3},
+        {name: 'Roscoe', age: 5, breed: 'Lab mix'}
+    ]
+}
